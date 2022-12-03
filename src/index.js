3f565b7afb3290825e5e193ead3f1c9b3c2e3a36
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-04 00:11:20 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ const DEBOUNCE_DELAY = 300;
 
 fetchCountries();
 function fetchCountries(name) {
-  return fetch('https://restcountries.com/v3.1/name/sweden')
+  return fetch('https://restcountries.com/v3.1/name/Ukraine')
     .then(responce => {
       // console.log(responce.json());
       return responce.json();
     })
     .then(data => {
-      console.log(data[0]);
+      console.log('All information:', data[0]);
+      console.log('The capital is ', data[0].capital);
+      console.log('Population is ', data[0].population, 'peoples');
+      console.log('Country language is', data[0].languages);
     });
 }
