1638a83d5955d98c7eebb9d135361d64b05b9fce
vladisulian <106911452+vladisulian@users.noreply.github.com>
2022-12-03 22:07:17 +0000

    Deploying to gh-pages from @ vladisulian/goit-js-hw-10@7a72fbfe03a916920f18109378a5229273bf4976 🚀

---
+++ index.5c9f3875.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/sweden").then((function(n){return n.json()})).then((function(n){console.log(n[0])}));
+//# sourceMappingURL=index.5c9f3875.js.map
---
+++ index.c856cae6.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/sweden").then((e=>e.json())).then((e=>{console.log(e[0])}));
+//# sourceMappingURL=index.c856cae6.js.map
