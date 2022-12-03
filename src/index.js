7a72fbfe03a916920f18109378a5229273bf4976
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-04 00:06:51 +0200

    Installing all pockets what I need.
    Add possibility to read info from backend

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries();
+function fetchCountries(name) {
+  return fetch('https://restcountries.com/v3.1/name/sweden')
+    .then(responce => {
+      // console.log(responce.json());
+      return responce.json();
+    })
+    .then(data => {
+      console.log(data[0]);
+    });
+}
